d20df802cb2d4b4444204787348edd8d7cfb09e6
phantrungg9 <vantrung16701@gmail.com>
2026-04-19 21:13:20 +0700

    trang history - notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.doanappfood"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.doanappfood"
